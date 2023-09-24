0b7b43b7a1b02665e2549c3403f38d8afc78ae99
Alexandre VICTOOR <alexandrevictoor@mbp-de-admin-2.lan>
2023-09-24 17:46:46 +0200

    Improve types

--- options.ts
+++ options.ts
@@ -18,20 +18,20 @@ export const some = <T>(value: T): Some<T> => ({
 
 export const O = {
   isSome: <T>(o: Option<T>): o is Some<T> => o._tag === 'Some',
-  getOrElse: <T>(fn: () => T) => (o: Option<T>) => O.isSome(o) ? o.value : fn(),
-  fromNullable: <T>(value: T) => (value === undefined || value === null) ? none : some(value),
-  fromPredicate: <T>(predicate: (value: T) => boolean) => (value: T) => predicate(value) ? some(value) : none,
-  match: <T, S>(onNone: () => S, onSome: (value: T) => S) => (o: Option<T>) => O.isSome(o) ? onSome(o.value) : onNone(),
-  map: <T, S>(fn: (value: T) => S) => (o: Option<T>) => O.isSome(o) ? some(fn(o.value)) : none,
-  filter:  <T>(predicate: (value: T) => boolean) => (o: Option<T>) => O.isSome(o) ? predicate(o.value) ? o : none : none,
-  filterMap: <T, S>(fn: (value: T) => Option<S>) => (o: Option<T>) => O.isSome(o) ? O.isSome(fn(o.value)) ? fn(o.value) : none : none,
+  getOrElse: <T>(fn: () => T) => (o: Option<T>): T => O.isSome(o) ? o.value : fn(),
+  fromNullable: <T>(value: T): Option<T> => (value === undefined || value === null) ? none : some(value),
+  fromPredicate: <T>(predicate: (value: T) => boolean) => (value: T): Option<T> => predicate(value) ? some(value) : none,
+  match: <T, S>(onNone: () => S, onSome: (value: T) => S) => (o: Option<T>): S => O.isSome(o) ? onSome(o.value) : onNone(),
+  map: <T, S>(fn: (value: T) => S) => (o: Option<T>): Option<S> => O.isSome(o) ? some(fn(o.value)) : none,
+  filter:  <T>(predicate: (value: T) => boolean) => (o: Option<T>): Option<T> => O.isSome(o) ? predicate(o.value) ? o : none : none,
+  filterMap: <T, S>(fn: (value: T) => Option<S>) => (o: Option<T>): Option<S> => O.isSome(o) ? O.isSome(fn(o.value)) ? fn(o.value) : none : none,
 };
 
 export const A = {
   map: <T, S>(fn: (item: T) => S) => (d: T[]) => d.map(fn),
+  reduce: <T, S>(init: S, fn: (acc: S, value: T) => S) => (array: Array<T>) => array.reduce(fn, init),
   findFirst: <T>(predicate: (value: T) => boolean) => (array: Array<T>): Option<T> => array.some(predicate) ? some(array.find(predicate) as T) : none,
   compact: <T>(array: Array<Option<T>>): Array<T> => array.filter(O.isSome).map((o: Some<T>) => o.value),
-  reduce: <T, S>(init: S, fn: (acc: S, value: T) => S) => (array: Array<T>) => array.reduce(fn, init)
 }
 
 export const R = {
